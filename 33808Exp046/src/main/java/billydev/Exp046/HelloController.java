b9fc01a6b08f28a105d044507f89a8016677f0e8
billy <billydev@gmail.com>
2020-04-06 12:59:20 -0600

    exp046: basic @RequestMapping stuff

--- 33808Exp046/src/main/java/billydev/Exp046/HelloController.java
+++ 33808Exp046/src/main/java/billydev/Exp046/HelloController.java
@@ -1,12 +1,30 @@
 package billydev.Exp046;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class HelloController {
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping(value = "/ex/foos", headers = "key=val", method = GET)
+    @ResponseBody
+    public String getFoosWithHeader() {
+        return "Get some Foos with Header";
+    }
+
+    @RequestMapping(
+            value = "/ex/foos",
+            method = GET,
+            headers = "Accept=application/json")
+    @ResponseBody
+    public String getFoosAsJsonFromBrowser() {
+        return "Get some Foos with Header Old";
     }
 }
