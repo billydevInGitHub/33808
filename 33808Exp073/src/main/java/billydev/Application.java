f7b11a3b01b3f0078dc96d0f8516d13e2507288f
billy <billydev@gmail.com>
2020-05-27 12:31:30 -0600

    Exp073: initial setup the Exp

---
+++ 33808Exp073/src/main/java/billydev/Application.java
@@ -0,0 +1,28 @@
+package billydev;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application  implements CommandLineRunner {
+    @Autowired
+    private Service service;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+    /**
+     * Callback used to run the bean.
+     *
+     * @param args incoming main method arguments
+     * @throws Exception on error
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        service.serve();
+        service.generalServe();
+    }
+}
