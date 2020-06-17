109d88c9ba07032832ca8eb924b18972ae2c9459
billy <billydev@gmail.com>
2020-06-17 11:04:35 -0600

    Exp081: initial setup Exp

---
+++ 33808Exp081/src/main/java/billydev/AppMain.java
@@ -0,0 +1,33 @@
+package billydev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AppMain {
+    //adding additional converters
+    @Bean
+    public HttpMessageConverters additionalConverters() {
+        return new HttpMessageConverters(new TheCustomConverter());
+    }
+
+    //replacing default converters
+    /*@Bean
+    public HttpMessageConverters converters() {
+        return new HttpMessageConverters(
+                false, Arrays.asList(new TheCustomConverter()));
+    }*/
+
+    /* registering additional converter
+    @Bean
+    public HttpMessageConverter<?> messageConverter(){
+        return new TheCustomConverter();
+    }*/
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppMain.class, args);
+    }
+}
---
+++ 33808Exp081/src/main/java/billydev/TheCustomConverter.java
@@ -0,0 +1,7 @@
+package billydev;
+
+import org.springframework.http.converter.StringHttpMessageConverter;
+
+public class TheCustomConverter extends StringHttpMessageConverter {
+    //just an empty custom converter
+}
