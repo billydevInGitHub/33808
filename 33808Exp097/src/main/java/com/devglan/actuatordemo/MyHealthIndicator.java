adee725d4935718078154b9a8f6b5c9633022b7f
billy <billydev@gmail.com>
2020-07-12 12:02:35 -0600

    Exp097: add custom health indicator

---
+++ 33808Exp097/src/main/java/com/devglan/actuatordemo/MyHealthIndicator.java
@@ -0,0 +1,25 @@
+package com.devglan.actuatordemo;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class MyHealthIndicator  implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        int errorCode = check(); // perform some specific health check
+        if (errorCode != 0) {
+            return Health.down().withDetail("Error Code", errorCode).build();
+        }
+        return Health.up().build();
+    }
+
+    private int check() {
+        return (new Random().nextInt())%2;
+    }
+
+}
