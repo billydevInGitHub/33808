b717c7ac4c6db35e74e8c39e0175e8a3a6131096
billy <billydev@gmail.com>
2020-07-16 20:19:32 -0600

    Exp113: initial setup of Exp

---
+++ 33808Exp113/src/main/java/com/sidd/nbs/Job.java
@@ -0,0 +1,26 @@
+package com.sidd.nbs;
+
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.Date;
+
+/**
+ * Created by Siddharth on 4/4/18.
+ */
+public class Job implements Runnable{
+
+    DeferredResult<String> deferredResult;
+    long processingTime;
+
+    public Job(DeferredResult<String> deferredResult, long processingTime)
+    {
+        this.deferredResult = deferredResult;
+        this.processingTime = processingTime;
+    }
+    @Override
+    public void run()
+    {
+        String result = "SUCCESS   /process-non-blocking completed in " + processingTime + " Ms"+ new Date().toString()+" ";
+        deferredResult.setResult(result);
+    }
+}
