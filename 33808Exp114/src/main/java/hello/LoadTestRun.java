385554e81550e8923fb622cf82fc37dce43739aa
billy <billydev@gmail.com>
2020-07-17 18:58:59 -0600

    Exp114: initial setup of Exp

---
+++ 33808Exp114/src/main/java/hello/LoadTestRun.java
@@ -0,0 +1,18 @@
+package hello;
+
+import com.sun.xml.internal.ws.spi.db.DatabindingException;
+
+import java.util.Date;
+
+public class LoadTestRun {
+    public static void main(String[] args) {
+
+        System.out.println("client start time:"+ new Date().toString());
+        int threadNumber=3000;
+        for (int i = 0; i < threadNumber; i++) {
+            new Thread(()->{
+                System.out.println(new GreetingWebClient().getResult());
+            }).start();
+        }
+    }
+}
