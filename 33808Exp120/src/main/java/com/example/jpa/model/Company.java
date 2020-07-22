a3ab5aa6c0b7e1f8d6b61b88398d9c00dec7e3a4
billy <billydev@gmail.com>
2020-07-21 20:32:10 -0600

    Exp120: initial setup of Exp, company table created

---
+++ 33808Exp120/src/main/java/com/example/jpa/model/Company.java
@@ -0,0 +1,36 @@
+package com.example.jpa.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "company")
+public class Company implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "name")
+	private String name;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
