125c86f9911e45e26ed66f2ec65a8b00c5acfdbe
billy <billydev@gmail.com>
2020-09-17 19:34:19 -0600

    Exp152: add metrics timer to record requests

--- 33808Exp152/Shared/src/main/java/billydev/Constants.java
+++ 33808Exp152/Shared/src/main/java/billydev/Constants.java
@@ -1,6 +1,7 @@
 package billydev;
 
 public class Constants {
-    public static final int THREAD_NUMBER = 3000;
-    public static final int THREAD_SLEEP_TIME=1000;
+    public static final int THREAD_NUMBER = 100;
+    public static final int THREAD_SLEEP_TIME=100;
+    public static final String REQUEST_TIMER_NAME = "REQUEST_TIMER";
 }
--- 33808Exp152/WebFluxApp/src/main/java/hello/GreetingHandler.java
+++ 33808Exp152/WebFluxApp/src/main/java/hello/GreetingHandler.java
@@ -2,6 +2,7 @@ package hello;
 
 import billydev.Constants;
 import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,10 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
+import static billydev.Constants.REQUEST_TIMER_NAME;
+
 @Component
 public class GreetingHandler {
 
@@ -18,14 +23,21 @@ public class GreetingHandler {
 	@Autowired
 	Counter counter;
 
+	@Autowired
+	MeterRegistry meterRegistry;
+
 	public Mono<ServerResponse> hello(ServerRequest request) {
 		counter.increment();
+		long startTime = System.currentTimeMillis();
 		try {
 			Thread.sleep(Constants.THREAD_SLEEP_TIME);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
-			.body(BodyInserters.fromValue("Hello, Spring!"));
+		Mono<ServerResponse> bodyReturn = ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
+				.body(BodyInserters.fromValue("Hello, Spring!"));
+		long endTime = System.currentTimeMillis();
+		meterRegistry.timer(REQUEST_TIMER_NAME).record(Duration.ofMillis(endTime - startTime));
+		return bodyReturn;
 	}
 }
