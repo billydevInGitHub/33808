75dda55db13a36fa804645f12405c489619a326c
billy <billydev@gmail.com>
2020-10-15 15:03:55 -0600

    Exp100: initial setup of Exp

---
+++ 33808Exp100/src/main/java/billydev/JmxEndpointExampleMain.java
@@ -0,0 +1,11 @@
+package billydev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmxEndpointExampleMain {
+    public static void main(String[] args) {
+        SpringApplication.run(JmxEndpointExampleMain.class);
+    }
+}
