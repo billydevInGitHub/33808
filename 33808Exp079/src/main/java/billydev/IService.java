f2da58e2970c50b043a43ff9c6f9310b7b0f46e7
billy <billydev@gmail.com>
2020-10-21 11:11:24 -0600

    Exp079: custom annotation must be on method implementation not on interface

--- 33808Exp079/src/main/java/billydev/IService.java
+++ 33808Exp079/src/main/java/billydev/IService.java
@@ -4,9 +4,7 @@ import org.springframework.stereotype.Component;
 
 //@Component
 public interface IService {
-    @LogExecutionTime
     void serve() throws InterruptedException;
 
-    @LogExecutionTime
     String generalServe();
 }
