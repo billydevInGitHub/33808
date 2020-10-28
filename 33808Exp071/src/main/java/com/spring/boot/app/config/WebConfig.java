e2989b79e78a96934800df70e608f8c506b9cce9
billy <billydev@gmail.com>
2020-10-28 11:15:08 -0600

    Exp071: webConfig directly implement WebMvcConfigurer instead of extend the adapter

--- 33808Exp071/src/main/java/com/spring/boot/app/config/WebConfig.java
+++ 33808Exp071/src/main/java/com/spring/boot/app/config/WebConfig.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -19,7 +20,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
  */
 
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
