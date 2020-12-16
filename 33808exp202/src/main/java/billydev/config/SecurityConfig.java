f4904a2a1d44b4309b3d0523ee0d48dfb625f5cf
billy <billydev@gmail.com>
2020-12-16 10:40:30 -0700

    exp202: add url access control based on roles.

--- 33808exp202/src/main/java/billydev/config/SecurityConfig.java
+++ 33808exp202/src/main/java/billydev/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package billydev.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
+import java.io.PrintWriter;
 
 /**
  * @author Billy
@@ -54,16 +56,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/js/**", "/css/**", "/images/**","/class/**");
+        web.ignoring().antMatchers("/js/**", "/css/**", "/images/**","/class/**","/login.html");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/actuator/**").hasRole("admin")
-                .antMatchers("/student/**").hasRole("student")
-                .anyRequest().authenticated()
+        http.csrf().disable() //禁用跨站csrf攻击防御，后面的章节会专门讲解
+                .formLogin()
+                .loginPage("/login.html")//用户未登录时，访问任何资源都转跳到该路径，即登录页面
+//                .loginProcessingUrl("/login")//登录表单form中action的地址，也就是处理认证请求的路径
+                .usernameParameter("uname")///登录表单form中用户名输入框input的name名，不修改的话默认是username
+                .passwordParameter("pword")//form中密码输入框input的name名，不修改的话默认是password
+                .defaultSuccessUrl("/class/1")//登录认证成功后默认转跳的路径
+                .loginProcessingUrl("/doLogin")
+                .successHandler((req, resp, authentication) -> {
+                    Object principal = authentication.getPrincipal();
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write(new ObjectMapper().writeValueAsString(principal));
+                    out.flush();
+                    out.close();
+                })
+                .failureHandler((req, resp, e) -> {
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write(e.getMessage());
+                    out.flush();
+                    out.close();
+                })
+                .permitAll()
                 .and()
-                .formLogin();
+                .authorizeRequests()
+                .antMatchers("/login.html","/login").permitAll()//不需要通过登录验证就可以被访问的资源路径
+                .antMatchers("/actuator/**").hasRole("admin") //前面是资源的访问路径、后面是资源的名称或者叫资源ID
+                .antMatchers("/student/**").hasRole("student")
+                .anyRequest().authenticated();
+
     }
 }
