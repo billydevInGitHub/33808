da755ec34bc0368e60df1ccacad09be432b7015e
billy <billydev@gmail.com>
2020-12-20 11:51:43 -0700

    exp204: update employee with exchange call.

--- 33808exp204/src/main/java/com/example/accessingdatamongodb/WebfluxFunctionalApp.java
+++ 33808exp204/src/main/java/com/example/accessingdatamongodb/WebfluxFunctionalApp.java
@@ -26,6 +26,9 @@ public class WebfluxFunctionalApp {
     @Bean
     public CommandLineRunner run() throws Exception {
         return args -> {
+            /*
+            create new Employee
+             */
             Employee employee = new Employee();
             employee.setId(18);
             employee.setName("testName18");
@@ -37,14 +40,39 @@ public class WebfluxFunctionalApp {
             webClient.post()
                     .uri("http://localhost:8080//create")
                     .body(Mono.just(employee), Employee.class).retrieve().bodyToMono(Object.class).subscribe();
-            System.out.println("Command line run");
 
 
-            System.out.println(webClient.get()
+            /*
+            get all employee information
+             */
+            System.out.println("first print:"+webClient.get()
                     .uri("http://localhost:8080")
                     .retrieve()
                     .bodyToFlux(Employee.class)
                     .collectList().block());
+
+            /*
+            do the update of employee
+             */
+            employee = new Employee();
+            employee.setId(19);
+            employee.setName("updated Name of 18");
+            employee.setSalary(19000);
+            webClient.put().uri("http://localhost:8080//update")
+                    .body(Mono.just(employee), Employee.class)
+                    .exchange().subscribe();
+
+
+                        /*
+            get all employee information
+             */
+            System.out.println("second print:"+webClient.get()
+                    .uri("http://localhost:8080")
+                    .retrieve()
+                    .bodyToFlux(Employee.class)
+                    .collectList().block());
+
+            System.out.println("Command line run completed");
         };
     }
 }
