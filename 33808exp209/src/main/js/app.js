6b6774a409745f3c0d9c1dd8fb95c0a5d2ffa049
billy <billydev@gmail.com>
2020-12-25 20:19:33 -0700

    exp209: after replace rest with axios and basically web application launched.

--- 33808exp209/src/main/js/app.js
+++ 33808exp209/src/main/js/app.js
@@ -1,11 +1,8 @@
 'use strict';
-
+const axios = require('axios');
 const React = require('react');
 const ReactDOM = require('react-dom');
-const when = require('when');
-const client = require('./client');
 
-const follow = require('./follow'); // function to hop multiple links by "rel"
 
 const stompClient = require('./websocket-listener');
 
@@ -27,45 +24,14 @@ class App extends React.Component {
 	}
 
 	loadFromServer(pageSize) {
-		follow(client, root, [
-				{rel: 'employees', params: {size: pageSize}}]
-		).then(employeeCollection => {
-			return client({
-				method: 'GET',
-				path: employeeCollection.entity._links.profile.href,
-				headers: {'Accept': 'application/schema+json'}
-			}).then(schema => {
-				// tag::json-schema-filter[]
-				/**
-				 * Filter unneeded JSON Schema properties, like uri references and
-				 * subtypes ($ref).
-				 */
-				Object.keys(schema.entity.properties).forEach(function (property) {
-					if (schema.entity.properties[property].hasOwnProperty('format') &&
-						schema.entity.properties[property].format === 'uri') {
-						delete schema.entity.properties[property];
-					}
-					else if (schema.entity.properties[property].hasOwnProperty('$ref')) {
-						delete schema.entity.properties[property];
-					}
-				});
-
-				this.schema = schema.entity;
-				this.links = employeeCollection.entity._links;
-				return employeeCollection;
-				// end::json-schema-filter[]
-			});
-		}).then(employeeCollection => {
-			this.page = employeeCollection.entity.page;
-			return employeeCollection.entity._embedded.employees.map(employee =>
-					client({
-						method: 'GET',
-						path: employee._links.self.href
-					})
-			);
-		}).then(employeePromises => {
-			return when.all(employeePromises);
-		}).done(employees => {
+	        axios.get('http://localhost:8080/employees')
+                .then(res => {
+                    this.setState({ usersCollection: res.data });
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+		done(employees => {
 			this.setState({
 				page: this.page,
 				employees: employees,
@@ -78,135 +44,135 @@ class App extends React.Component {
 
 	// tag::on-create[]
 	onCreate(newEmployee) {
-		follow(client, root, ['employees']).done(response => {
-			client({
-				method: 'POST',
-				path: response.entity._links.self.href,
-				entity: newEmployee,
-				headers: {'Content-Type': 'application/json'}
-			})
-		})
+//		follow(client, root, ['employees']).done(response => {
+//			client({
+//				method: 'POST',
+//				path: response.entity._links.self.href,
+//				entity: newEmployee,
+//				headers: {'Content-Type': 'application/json'}
+//			})
+//		})
 	}
 	// end::on-create[]
 
 	// tag::on-update[]
 	onUpdate(employee, updatedEmployee) {
-		if(employee.entity.manager.name === this.state.loggedInManager) {
-			updatedEmployee["manager"] = employee.entity.manager;
-			client({
-				method: 'PUT',
-				path: employee.entity._links.self.href,
-				entity: updatedEmployee,
-				headers: {
-					'Content-Type': 'application/json',
-					'If-Match': employee.headers.Etag
-				}
-			}).done(response => {
-				/* Let the websocket handler update the state */
-			}, response => {
-				if (response.status.code === 403) {
-					alert('ACCESS DENIED: You are not authorized to update ' +
-						employee.entity._links.self.href);
-				}
-				if (response.status.code === 412) {
-					alert('DENIED: Unable to update ' + employee.entity._links.self.href +
-						'. Your copy is stale.');
-				}
-			});
-		} else {
-			alert("You are not authorized to update");
-		}
+//		if(employee.entity.manager.name === this.state.loggedInManager) {
+//			updatedEmployee["manager"] = employee.entity.manager;
+//			client({
+//				method: 'PUT',
+//				path: employee.entity._links.self.href,
+//				entity: updatedEmployee,
+//				headers: {
+//					'Content-Type': 'application/json',
+//					'If-Match': employee.headers.Etag
+//				}
+//			}).done(response => {
+//				/* Let the websocket handler update the state */
+//			}, response => {
+//				if (response.status.code === 403) {
+//					alert('ACCESS DENIED: You are not authorized to update ' +
+//						employee.entity._links.self.href);
+//				}
+//				if (response.status.code === 412) {
+//					alert('DENIED: Unable to update ' + employee.entity._links.self.href +
+//						'. Your copy is stale.');
+//				}
+//			});
+//		} else {
+//			alert("You are not authorized to update");
+//		}
 	}
 	// end::on-update[]
 
 	// tag::on-delete[]
 	onDelete(employee) {
-		client({method: 'DELETE', path: employee.entity._links.self.href}
-		).done(response => {/* let the websocket handle updating the UI */},
-		response => {
-			if (response.status.code === 403) {
-				alert('ACCESS DENIED: You are not authorized to delete ' +
-					employee.entity._links.self.href);
-			}
-		});
+//		client({method: 'DELETE', path: employee.entity._links.self.href}
+//		).done(response => {/* let the websocket handle updating the UI */},
+//		response => {
+//			if (response.status.code === 403) {
+//				alert('ACCESS DENIED: You are not authorized to delete ' +
+//					employee.entity._links.self.href);
+//			}
+//		});
 	}
 	// end::on-delete[]
 
 	onNavigate(navUri) {
-		client({
-			method: 'GET',
-			path: navUri
-		}).then(employeeCollection => {
-			this.links = employeeCollection.entity._links;
-			this.page = employeeCollection.entity.page;
-
-			return employeeCollection.entity._embedded.employees.map(employee =>
-					client({
-						method: 'GET',
-						path: employee._links.self.href
-					})
-			);
-		}).then(employeePromises => {
-			return when.all(employeePromises);
-		}).done(employees => {
-			this.setState({
-				page: this.page,
-				employees: employees,
-				attributes: Object.keys(this.schema.properties),
-				pageSize: this.state.pageSize,
-				links: this.links
-			});
-		});
+//		client({
+//			method: 'GET',
+//			path: navUri
+//		}).then(employeeCollection => {
+//			this.links = employeeCollection.entity._links;
+//			this.page = employeeCollection.entity.page;
+//
+//			return employeeCollection.entity._embedded.employees.map(employee =>
+//					client({
+//						method: 'GET',
+//						path: employee._links.self.href
+//					})
+//			);
+//		}).then(employeePromises => {
+//			return when.all(employeePromises);
+//		}).done(employees => {
+//			this.setState({
+//				page: this.page,
+//				employees: employees,
+//				attributes: Object.keys(this.schema.properties),
+//				pageSize: this.state.pageSize,
+//				links: this.links
+//			});
+//		});
 	}
 
 	updatePageSize(pageSize) {
-		if (pageSize !== this.state.pageSize) {
-			this.loadFromServer(pageSize);
-		}
+//		if (pageSize !== this.state.pageSize) {
+//			this.loadFromServer(pageSize);
+//		}
 	}
 
 	// tag::websocket-handlers[]
 	refreshAndGoToLastPage(message) {
-		follow(client, root, [{
-			rel: 'employees',
-			params: {size: this.state.pageSize}
-		}]).done(response => {
-			if (response.entity._links.last !== undefined) {
-				this.onNavigate(response.entity._links.last.href);
-			} else {
-				this.onNavigate(response.entity._links.self.href);
-			}
-		})
+//		follow(client, root, [{
+//			rel: 'employees',
+//			params: {size: this.state.pageSize}
+//		}]).done(response => {
+//			if (response.entity._links.last !== undefined) {
+//				this.onNavigate(response.entity._links.last.href);
+//			} else {
+//				this.onNavigate(response.entity._links.self.href);
+//			}
+//		})
 	}
 
 	refreshCurrentPage(message) {
-		follow(client, root, [{
-			rel: 'employees',
-			params: {
-				size: this.state.pageSize,
-				page: this.state.page.number
-			}
-		}]).then(employeeCollection => {
-			this.links = employeeCollection.entity._links;
-			this.page = employeeCollection.entity.page;
-
-			return employeeCollection.entity._embedded.employees.map(employee => {
-				return client({
-					method: 'GET',
-					path: employee._links.self.href
-				})
-			});
-		}).then(employeePromises => {
-			return when.all(employeePromises);
-		}).then(employees => {
-			this.setState({
-				page: this.page,
-				employees: employees,
-				attributes: Object.keys(this.schema.properties),
-				pageSize: this.state.pageSize,
-				links: this.links
-			});
-		});
+//		follow(client, root, [{
+//			rel: 'employees',
+//			params: {
+//				size: this.state.pageSize,
+//				page: this.state.page.number
+//			}
+//		}]).then(employeeCollection => {
+//			this.links = employeeCollection.entity._links;
+//			this.page = employeeCollection.entity.page;
+//
+//			return employeeCollection.entity._embedded.employees.map(employee => {
+//				return client({
+//					method: 'GET',
+//					path: employee._links.self.href
+//				})
+//			});
+//		}).then(employeePromises => {
+//			return when.all(employeePromises);
+//		}).then(employees => {
+//			this.setState({
+//				page: this.page,
+//				employees: employees,
+//				attributes: Object.keys(this.schema.properties),
+//				pageSize: this.state.pageSize,
+//				links: this.links
+//			});
+//		});
 	}
 	// end::websocket-handlers[]
 
